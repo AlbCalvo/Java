afa0084026a024738c02270112e23b7997539707
“Alberto <acmmin@gmail.com>
2023-10-22 17:05:38 +0200

    Borrado ejercicios de prueba en java

--- jpaDClaseATabla/src/main/java/com/mycompany/jpadclaseatabla/persistencia/ControladoraPersistencia.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.jpadclaseatabla.persistencia;
-
-public class ControladoraPersistencia {
-    AlumnoJpaController alujpa = new AlumnoJpaController();
-    
-    
-}
--- jpaDClaseATabla/target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mycompany.jpadclaseatabla"},{"l":"com.mycompany.jpadclaseatabla.logica"},{"l":"com.mycompany.jpadclaseatabla.persistencia"},{"l":"com.mycompany.jpadclaseatabla.persistencia.exceptions"}];updateSearchResults();
